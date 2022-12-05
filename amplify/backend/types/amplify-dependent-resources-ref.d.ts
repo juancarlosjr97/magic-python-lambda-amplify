cd617c8cd94a00ee5c6ebd5e1a67ce180eee7afe
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2022-12-05 23:49:59 +0000

    Adding project for a lambda and an API Gateway using AWS Amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "magiclambda": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "magicapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
